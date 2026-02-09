81f0538639c0870b87fb5c99ceec720e009e615a
Gigi Albers <gigi_albers@mines.edu>
2026-02-09 13:14:50 -0700

    Add my first python script

---
+++ short-assignments/python-intro.py
@@ -0,0 +1,10 @@
+
+# Learning how to write a Python script - GPGN268
+# Author: Gigi Albers
+# Date: Febuary 9, 2026
+
+# Temperature in degrees Fahrenheit
+temperature = 55
+# Location of the measurement
+city = "Golden"
+print("It is", temperature, "F", "in", city)
